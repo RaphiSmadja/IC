92da7c3230f66273c243013881c1b7d97e953d3f
Unknown <quentin.beranger@gmail.com>
2018-02-06 16:38:39 +0100

    up

--- src/test/java/com/example/javamavenjunithelloworld/HelloTest.java
+++ src/test/java/com/example/javamavenjunithelloworld/HelloTest.java
@@ -18,11 +18,7 @@ public class HelloTest {
 
 
 
-    @Test(expected = IllegalArgumentException.class)
-    public void testIllegalArgumentForHello21() {
-        Hello hi = new Hello();
-        hi.setTimes(Hello.MAXIMUM_AMOUNT_OF_TIMES + 1);
-    }
+    
 
 
 }
